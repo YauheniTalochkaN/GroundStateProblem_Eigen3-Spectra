0e6c18106d25b85cac0ebbb1e4113f4a1ef75e38
YauheniTalochkaN <yauheni.talochka@gmail.com>
2025-12-20 00:50:45 +0300

    test

--- main.cc
+++ main.cc
@@ -46,8 +46,8 @@ inline int get_flipped_state(int state, int i, int j)
 
 void build_sparse_hamiltonian(int N, int dim,   
                               const std::vector<Edge>& edges,
-                              Eigen::SparseMatrix<complexType>& H1z,
-                              Eigen::SparseMatrix<complexType>& H) 
+                              Eigen::SparseMatrix<complexType, Eigen::ColMajor>& H1z,
+                              Eigen::SparseMatrix<complexType, Eigen::ColMajor>& H) 
 {       
     for (int state = 0; state < dim; ++state) 
     {
@@ -59,9 +59,12 @@ void build_sparse_hamiltonian(int N, int dim,
 
             complexType sign_i = (bit_i == 0) ? complexType(1.0, 0.0) : complexType(-1.0, 0.0);
 
-            /* H1x.coeffRef(flipped_state, state) += complexType(1.0, 0.0);
-            
-            H1y.coeffRef(flipped_state, state) += complexType(0.0, 1.0) * sign_i; */
+            /* if(flipped_state < state)
+            {
+                H1x.coeffRef(flipped_state, state) += complexType(1.0, 0.0);
+
+                H1y.coeffRef(flipped_state, state) += complexType(0.0, 1.0) * sign_i;
+            } */
 
             H1z.coeffRef(state, state) += sign_i;
         }
@@ -83,9 +86,12 @@ void build_sparse_hamiltonian(int N, int dim,
             complexType sign_i = (bit_i == 0) ? complexType(1.0, 0.0) : complexType(-1.0, 0.0);
             complexType sign_j = (bit_j == 0) ? complexType(1.0, 0.0) : complexType(-1.0, 0.0);
             
-            H.coeffRef(flipped_state, state) += complexType(Jx, 0.0);
-            
-            H.coeffRef(flipped_state, state) += complexType(-Jy, 0.0) * sign_i * sign_j;
+            if(flipped_state < state)
+            {
+                H.coeffRef(flipped_state, state) += complexType(Jx, 0.0);
+
+                H.coeffRef(flipped_state, state) += complexType(-Jy, 0.0) * sign_i * sign_j;
+            }
             
             H.coeffRef(state, state) += complexType(Jz, 0.0) * sign_i * sign_j;
         }
@@ -93,19 +99,18 @@ void build_sparse_hamiltonian(int N, int dim,
 }
 
 int main(int argc, char* argv[]) 
-{
-    omp_set_num_threads(20);
-    
+{    
     Eigen::initParallel();
-    Eigen::setNbThreads(omp_get_max_threads());
+    omp_set_num_threads(20);
+    Eigen::setNbThreads(20);
     
-    const int N = 24;
+    const int N = 14;
     const int DIM = 1 << N;
 
     const double Junit = 170.0;
     const double gmuB = (1.0 / 0.086) * 0.05788 * 2.0;
 
-    /* const double J1 = 0.012;
+    const double J1 = 0.012;
     const double J2 = 0.694;
     const double J3 = 0.971;
     const double J4 = 1.000;
@@ -117,15 +122,15 @@ int main(int argc, char* argv[])
     std::vector<std::pair<int, int>> J3edges = {{2,6},{1,12},{4,6},{3,5},{9,13},{8,5},{11,13},{10,12}};
     std::vector<std::pair<int, int>> J4edges = {{1,3},{2,4},{8,10},{9,11}};
     std::vector<std::pair<int, int>> J5edges = {{1,7},{2,7},{3,7},{4,7},{8,14},{9,14},{10,14},{11,14}};
-    std::vector<std::pair<int, int>> J6edges = {{6,12},{6,12},{5,6},{5,6},{5,13},{5,13},{12,13},{12,13}}; */
+    std::vector<std::pair<int, int>> J6edges = {{6,12},{6,12},{5,6},{5,6},{5,13},{5,13},{12,13},{12,13}};
 
-    const double J1 = 1.0;
+    /* const double J1 = 1.0;
     const double J2 = 1.0;
     const double J3 = 1.0;
 
     std::vector<std::pair<int, int>> J1edges = {{1,18},{2,5},{3,6},{4,11},{7,24},{8,11},{9,12},{10,5},{13,6},{14,17},{15,18},{16,23},{19,12},{20,23},{21,24},{22,17}};
-    std::vector<std::pair<int, int>> J2edges = {{1,2}, {2,3},{3,4},{4,1},{7,8},{8,9},{9,10},{10,7},{13,14},{14,15},{15,16},{16,13},{19,20},{20,21},{21,22},{22,19}};
-    std::vector<std::pair<int, int>> J3edges = {{2,18},{1,11},{4,6},{3,5},{8,24},{7,5},{10,12},{9,11},{14,6},{13,23},{16,18},{15,17},{20,12},{19,17},{22,24},{21,23}};
+    std::vector<std::pair<int, int>> J2edges = {{1,2},{2,3},{3,4},{4,1},{7,8},{8,9},{9,10},{10,7},{13,14},{14,15},{15,16},{16,13},{19,20},{20,21},{21,22},{22,19}};
+    std::vector<std::pair<int, int>> J3edges = {{2,18},{1,11},{4,6},{3,5},{8,24},{7,5},{10,12},{9,11},{14,6},{13,23},{16,18},{15,17},{20,12},{19,17},{22,24},{21,23}}; */
 
     std::vector<Edge> Jedges;
 
@@ -140,14 +145,14 @@ int main(int argc, char* argv[])
     add_group(J1edges, J1, J1, J1);
     add_group(J2edges, J2, J2, J2);
     add_group(J3edges, J3, J3, J3);
-    /* add_group(J4edges, J4, J4, J4);
+    add_group(J4edges, J4, J4, J4);
     add_group(J5edges, J5, J5, J5);
-    add_group(J6edges, J6, J6, J6); */
+    add_group(J6edges, J6, J6, J6);
 
     std::cout << "Building Hamiltonian...\n";
 
-    Eigen::SparseMatrix<complexType> H1z(DIM, DIM);
-    Eigen::SparseMatrix<complexType> H(DIM, DIM);
+    Eigen::SparseMatrix<complexType, Eigen::ColMajor> H1z(DIM, DIM);
+    Eigen::SparseMatrix<complexType, Eigen::ColMajor> H(DIM, DIM);
 
     build_sparse_hamiltonian(N, DIM, Jedges, H1z, H);
 
@@ -174,11 +179,9 @@ int main(int argc, char* argv[])
             H -= complexType(dBz * gmuB, 0.0) * H1z;
         }
 
-        H.makeCompressed();
+        Spectra::SparseHermMatProd<complexType, Eigen::Upper, Eigen::ColMajor> opH(H);
 
-        Spectra::SparseHermMatProd<complexType> opH(H);
-
-        Spectra::HermEigsSolver<Spectra::SparseHermMatProd<complexType>> eigsH(opH, 1, 10);
+        Spectra::HermEigsSolver<Spectra::SparseHermMatProd<complexType, Eigen::Upper, Eigen::ColMajor>> eigsH(opH, 1, 10);
     
         eigsH.init();
         int nconv = eigsH.compute(Spectra::SortRule::SmallestAlge);
